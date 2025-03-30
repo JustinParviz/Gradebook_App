6d64281cd7912125069962890be76c9d31bcc20f
JustinParviz <jarshravan@gmail.com>
2025-03-30 18:50:24 -0400

    Added the getGrade function which gives a certain grade for each score

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 
+// *** Gradebook App ***
+
+
 // function getAverage(scores) {
 //     let sum = 0;
 //     for (let i = 0; i < scores.length; i++) {
@@ -19,10 +22,26 @@ function getAverage(scores) {
     return average
 }
 
-console.log(getAverage([65, 88, 73, 97, 85, 81, 92, 78])) // Test scores worked!
+// console.log(getAverage([65, 88, 73, 97, 85, 81, 92, 78]))     // Test scores worked!
 
 
+function getGrade(score) {
+    if (score === 100) {
+      return `Your score is ${score}, your Grade is A++`;
+    } else if (score >= 90) {
+      return `Your score is ${score}, your Grade is A`;
+    } else if (score >= 80) {
+      return `Your score is ${score}, your Grade is B`;
+    } else if (score >= 70) {
+      return `Your score is ${score}, your Grade is C`;
+    } else if (score >= 60) {
+      return `Your score is ${score}, your Grade is D`;
+    } else {
+      return `Your score is ${score}, your Grade is F`;
+    }
+}
 
+// console.log(getGrade(81));     // Succefully received the correct Grade for any score!
 
 
 
