6499e07ddb4e3adb82dbd72c0fec17697009e1a1
JustinParviz <jarshravan@gmail.com>
2025-03-30 18:18:47 -0400

    created shortened version of getAverage function

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 
+// function getAverage(scores) {
+//     let sum = 0;
+//     for (let i = 0; i < scores.length; i++) {
+//         sum += scores[i];
+//     }
+//     let average = sum / scores.length;
+    
+//     return average
+// }
+
 function getAverage(scores) {
-    let sum = 0;
-    for (let i = 0; i < scores.length; i++) {
-      sum += scores[i];
+    let sum = 0
+    for (const score of scores) {
+        sum += score;
     }
     let average = sum / scores.length;
-      return average
-  }
 
-// console.log(getAverage([65, 88, 73, 97, 85, 81, 92, 78])) Test scores worked!
+    return average
+}
+
+console.log(getAverage([65, 88, 73, 97, 85, 81, 92, 78])) // Test scores worked!
+
+
+
+
+
+
