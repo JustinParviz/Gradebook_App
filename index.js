30d5bafb60b3364b730318fef04bc89ea96cbb68
JustinParviz <jarshravan@gmail.com>
2025-03-31 17:17:45 -0400

    Built the studentMsg function that returns a certain message based on the student's score and the class average

--- index.js
+++ index.js
@@ -13,6 +13,7 @@
 // }
 
 // - Created shortened version of the getAverage function, as seen below:
+// NOTE: getAverage is for the CLASS average.
 
 function getAverage(scores) {
     let sum = 0
@@ -43,14 +44,38 @@ function getGrade(score) {
     }
 }
 
-// console.log(getGrade(81));     // Succefully received the correct Grade for any score!
+// console.log(getGrade(81));     // Successfully received the correct Grade for any score!
 
 
 function hasPassingGrade(score) {
     return getGrade(score) !== "F";
 }
 
-console.log(hasPassingGrade(100));
-console.log(hasPassingGrade(53));
-console.log(hasPassingGrade(87));
+// Ran tests below to make sure the function works and that for all scores that are a passing 
+// grade, the function returns true. Otherwise the function returns false.  
+
+// console.log(hasPassingGrade(100));    // Returns: true
+// console.log(hasPassingGrade(53));     // Returns: false
+// console.log(hasPassingGrade(87));     // Returns: true
+
+
+function studentMsg(totalScores, studentScore) {
+
+    let classAverage = getAverage(totalScores);
+    let studentsGrade = getGrade(studentScore);
+    let passedCourse = hasPassingGrade(studentScore);
+
+    if (passedCourse === true) {
+        return "Class average: " + classAverage + ". Your grade: " + studentsGrade + ". You passed the course."
+    } else {
+        return "Class average: " + classAverage + ". Your grade: " + studentsGrade + ". You failed the course."
+    }
+}
+
+// Ran tests for function below and each test output returned the correct message based on the student's score and the class average.
+
+console.log(studentMsg([92, 88, 12, 77, 57, 100, 67, 38, 97, 89], 37));    // Returns: Class average: 71.7. Your grade: F. You failed the course.
+console.log(studentMsg([56, 23, 89, 42, 75, 11, 68, 34, 91, 19], 100));    // Returns: Class average: 50.8. Your grade: A++. You passed the course.
+
+
 
